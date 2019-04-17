7fbcbe4181dea0db9b767c13cc8d263881434f6f
Peter Tardif <peter.tardif@gmail.com>
2019-04-16 23:52:02 -0600

    cleaned up and slight refactor

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
 'use strict';
 
-
 function getDogImage(userInput) {
   fetch(`https://dog.ceo/api/breed/${userInput}/images/random`)
   .then(response => response.json())
-  .then(responseJson => {
-      displayResults(responseJson);
-  })
-  
+  .then(responseJson => 
+      displayResults(responseJson))
       .catch(error => alert('Something went wrong. Try again later.')); 
-  
   }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  //replace the existing image with the new one
-    
-  //   let temp=`<img src="${responseJson.message[i]}" class="results-img">`;
-  //   $('.results').append(temp);
-    
-  // }
   if (responseJson.status === "error"){
-    alert(responseJson.message);
+    $('.dogTitle').remove();
+    $('.results').append(
+      `Error code: ${responseJson.code}. ${responseJson.message}.`
+    )
   }
-  else{
-  
-  $('.results').append(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
+  else {
+    $('.results').append(
+      `<img src="${responseJson.message}" class="results-img">`
+    )
+  }
   //display the results section
   $('.results').removeClass('hidden');
 }
-}
+
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
